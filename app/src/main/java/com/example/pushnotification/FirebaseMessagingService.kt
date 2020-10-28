09cd2c36044bee94e9d2f876166238e259cbb35a
Kwounsu <sb00lgs@gmail.com>
2020-10-28 13:36:51 -0700

    FCM

---
+++ app/src/main/java/com/example/pushnotification/FirebaseMessagingService.kt
@@ -0,0 +1,42 @@
+package com.example.pushnotification
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
+import android.media.RingtoneManager
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+
+class FirebaseMessagingService : FirebaseMessagingService() {
+    private var msg: String? = null
+    private var title: String? = null
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        Log.e(TAG, "onMessageReceived")
+        title = remoteMessage.notification!!.title
+        msg = remoteMessage.notification!!.body
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val contentIntent = PendingIntent.getActivity(
+            this, 0, Intent(
+                this,
+                MainActivity::class.java
+            ), 0
+        )
+        val mBuilder = NotificationCompat.Builder(this).setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle(title)
+            .setContentText(msg)
+            .setAutoCancel(true)
+            .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+            .setVibrate(longArrayOf(1, 1000))
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(0, mBuilder.build())
+        mBuilder.setContentIntent(contentIntent)
+    }
+
+    companion object {
+        private const val TAG = "FirebaseMsgService"
+    }
+}
---
+++ app/src/main/java/com/example/pushnotification/FirebaseinstanceIDServices.kt
@@ -0,0 +1,20 @@
+package com.example.pushnotification
+
+import android.util.Log
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.iid.FirebaseInstanceIdService
+
+
+class FirebaseinstanceIDServices : FirebaseInstanceIdService() {
+    override fun onTokenRefresh() {
+        val token = FirebaseInstanceId.getInstance().token
+        Log.e(TAG, token)
+        sendRegistrationToServer(token)
+    }
+
+    private fun sendRegistrationToServer(token: String?) {}
+
+    companion object {
+        private const val TAG = "MyFirebaseIIDService"
+    }
+}
